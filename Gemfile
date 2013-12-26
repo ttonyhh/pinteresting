ae51c5651758edea2d0e74ce0b619f07ed7e4d55
Tony <ttonyhh@gmail.com>
2013-12-26 00:49:33 -0500

    Add image upload with paperclip

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'jbuilder', '~> 1.2'
 gem 'bootstrap-sass', :github => 'thomas-mcdonald/bootstrap-sass'
 
 gem 'devise', '~> 3.1.1'
+gem 'paperclip', '~> 3.0'
 
 group :development, :test do
 	gem 'sqlite3'
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -46,7 +46,7 @@ class PinsController < ApplicationController
     end
 
     def correct_user
-      @pin = current_user.pins.find(id: params[:id])
+      @pin = current_user.pins.find_by(id: params[:id])
       redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     end
 
