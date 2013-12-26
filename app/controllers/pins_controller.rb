7c8d6ef92a8e3879e523cf58a442f9f7b9ce06dd
Tony <ttonyhh@gmail.com>
2013-12-26 18:13:26 -0500

    commented out user restrictions to delete older posts

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -45,10 +45,10 @@ class PinsController < ApplicationController
       @pin = Pin.find(params[:id])
     end
 
-    def correct_user
-      @pin = current_user.pins.find_by(id: params[:id])
-      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
-    end
+    #def correct_user
+     # @pin = current_user.pins.find_by(id: params[:id])
+    #  redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+    #end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
